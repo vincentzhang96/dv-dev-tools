e96af307000c4dfd1656fedf430071bc9e522737
Vincent Zhang <vzhang@purdue.edu>
2018-10-05 00:08:26 -0700

    Release routing

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production' ?
+        '/tool-store/prod/dv-dev-tools/dev/'
+        : '/',
     transpileDependencies: [
         /\bvue-awesome\b/
     ]
